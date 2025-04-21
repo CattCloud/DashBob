0400313efa296cc367c807ca8b80047ecc0a4082
Erick Verde <erick.verde@unmsm.edu.pe>
2025-04-21 18:15:56 -0500

    edit app.js

--- js/app.js
+++ js/app.js
@@ -212,11 +212,11 @@ document.getElementById("form-ingreso").addEventListener("submit", (e) => {
 
 function getBadgeClase(estado) {
   const clases = {
-    PENDIENTE: "bg-yellow-100 text-yellow-800",
-    FACTURADO: "bg-blue-100 text-blue-800",
-    COMPLETADO: "bg-green-100 text-green-800",
-    DEVUELTO: "bg-gray-100 text-gray-800",
-    "SALDO A FAVOR": "bg-purple-100 text-purple-800"
+    pendiente: "bg-yellow-100 text-yellow-800",
+    facturado: "bg-blue-100 text-blue-800",
+    completado: "bg-green-100 text-green-800",
+    devuelto: "bg-gray-100 text-gray-800",
+    "saldo a favor": "bg-purple-100 text-purple-800"
   };
   return clases[estado] || "bg-gray-200 text-gray-800";
 }
